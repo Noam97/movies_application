2c22bfc77a07dc0cc96b6f98e506ed3041569667
Noam Lahmani <noamm240@gmail.com>
2023-01-10 21:34:33 +0200

    schemas

---
+++ schemas.py
@@ -0,0 +1,158 @@
+import csv
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='123456',
+                              host='127.0.0.1',
+                              port = "2589",
+                              database = "dbexample")
+cursor = cnx.cursor()
+
+"""Delete from tables"""
+try:
+    cursor.execute("delete from player")
+except mysql.connector.Error as err:
+    print("Failed creating database: {}".format(err))
+
+try:
+    cursor.execute("delete from knownfor")
+except mysql.connector.Error as err:
+    print("Failed creating database: {}".format(err))
+
+
+"""Create tables of the scheme"""
+
+# cursor.execute( "CREATE TABLE `dbexample`.`knownfor`\
+# ( `playerid` VARCHAR(45) NOT NULL,`movieid` VARCHAR(45) NOT NULL,\
+# PRIMARY KEY (`playerid`, `movieid`));")
+
+# cursor.execute("CREATE TABLE `knownfor` (\
+#   `playerid` varchar(45) NOT NULL,\
+#   `movieid` varchar(45) NOT NULL,\
+#   PRIMARY KEY (`playerid`,`movieid`)\
+# ) ENGINE=InnoDB DEFAULT CHARSET=latin1")
+
+
+# cursor.execute("CREATE TABLE `movie` (\
+#   `id` int NOT NULL AUTO_INCREMENT,\
+#   `movieid` varchar(45) NOT NULL,\
+#   `name` varchar(45) NOT NULL,\
+#   `genre` varchar(45) DEFAULT NULL,\
+#   `rating` varchar(45) DEFAULT NULL,\
+#   PRIMARY KEY (`id`)\
+# ) ENGINE=InnoDB AUTO_INCREMENT=1026378 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci")
+
+# cursor.execute("CREATE TABLE `player` (\
+#   `id` int NOT NULL AUTO_INCREMENT,\
+#   `playerid` varchar(45) NOT NULL,\
+#   `name` varchar(45) NOT NULL,\
+#   `birthyear` varchar(45) DEFAULT NULL,\
+#   `deathyear` varchar(45) DEFAULT NULL,\
+#   PRIMARY KEY (`id`),\
+#   UNIQUE KEY `id_UNIQUE` (`id`)\
+# ) ENGINE=InnoDB AUTO_INCREMENT=905979 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci")
+
+# cursor.execute("CREATE TABLE `rating` (\
+#   `id` int NOT NULL AUTO_INCREMENT,\
+#   `rate` int NOT NULL,\
+#   `comment` varchar(45) DEFAULT NULL,\
+#   `Movie_ID` int NOT NULL,\
+#   `userid` int DEFAULT NULL,\
+#   PRIMARY KEY (`id`)\
+# ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci")
+
+
+# cursor.execute("CREATE TABLE `users` (\
+#   `idusers` int NOT NULL AUTO_INCREMENT,\
+#   `username` varchar(45) NOT NULL,\
+#   `password` varchar(45) NOT NULL,\
+#   `email` varchar(256) NOT NULL,\
+#   `age` tinyint NOT NULL,\
+#   PRIMARY KEY (`idusers`),\
+#   UNIQUE KEY `idusers_UNIQUE` (`idusers`)\
+# ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1")
+
+
+
+
+with open("ImdbName.csv", 'r' , encoding = "utf8") as file:
+  next(iter(file))
+  csvreader = csv.reader(file)
+  counter = 0
+  cursor.execute("alter table player AUTO_INCREMENT = 1")
+  for row in csvreader:
+    playerid = row[0]
+    for movieid in row[-1].split(","):
+      try:
+        cursor.execute("insert into knownfor(playerid, movieid) values(%s, %s);",
+                       (playerid, movieid))
+      except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+    birthyear = row[2]
+    deathyear = row[3]
+    try:
+      birthyear = int(birthyear)
+    except ValueError:
+      birthyear = None
+    try:
+      deathyear = int(deathyear)
+    except ValueError:
+      deathyear = None
+    cursor.execute("insert into player(playerid, name, birthyear, deathyear) values(%s,%s,%s,%s);",
+                   (playerid,row[1],birthyear,deathyear))
+
+    counter+=1
+    # print(playerid, movieid)
+    # if counter >= 10:
+    #   break
+
+cnx.commit()
+cursor = cnx.cursor()
+
+
+# with open("ImdbTitleBasics.csv", 'r' , encoding = "utf8") as file:
+#     next(iter(file))
+#     csvreader = csv.reader(file)
+#     counter = 0
+#     cursor.execute("alter table basictable AUTO_INCREMENT = 1")
+#     for row in csvreader:
+#         genre = row[-1]
+#         if genre.lower() == "\\n":
+#             genre = None
+#         counter+=1
+#         # print(row)
+#         # if counter == 10:
+#         #   break
+#         try:
+#             cursor.execute("insert into movie(movieid, name, genre, rating) values(%s,  %s, %s,%s);",
+#                            (row[0],row[2], genre, None))
+#         except mysql.connector.Error as err:
+#             print("Failed creating database: {}".format(err))
+# cnx.commit()
+# cursor = cnx.cursor()
+
+
+"""
+with open("ImdbTitleAkas.csv", 'r' , encoding = "utf8") as file:
+    next(iter(file))
+    csvreader = csv.reader(file)
+    counter = 0
+    cursor.execute("alter table basictable AUTO_INCREMENT = 1")
+    for row in csvreader:
+        region = row[3]
+        movieid = row[0]
+        if region.lower() ==  "\\n":
+            region = None
+        counter += 1
+        print(row)
+        if counter == 10:
+            break
+        try:
+            cursor.execute("update movie set region = %s where movieid == %s;",(region,movieid))
+        except mysql.connector.Error as err:
+            print("Failed creating database: {}".format(err))
+
+cnx.commit()
+cursor = cnx.cursor()
+"""
+
+
---
+++ tables_from_kaggle.py
@@ -0,0 +1,131 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import csv
+import mysql.connector
+
+"""Create tables"""
+
+# cursor.execute("CREATE TABLE `basictable` (\
+#   `id` int NOT NULL AUTO_INCREMENT,\
+#   `moviename` varchar(512) NOT NULL,\
+#   `movietype` varchar(512) NOT NULL,\
+#   PRIMARY KEY (`id`),\
+#   UNIQUE KEY `id_UNIQUE` (`id`)\
+# ) ENGINE=InnoDB AUTO_INCREMENT=1049171 DEFAULT CHARSET=latin1")
+
+# cursor.execute("CREATE TABLE `imbdtitleakas` (\
+#   `id` int NOT NULL AUTO_INCREMENT,\
+#   `region` varchar(45) DEFAULT NULL,\
+#   `language` varchar(45) DEFAULT NULL,\
+#   PRIMARY KEY (`id`),\
+#   UNIQUE KEY `id_UNIQUE` (`id`)\
+# ) ENGINE=InnoDB AUTO_INCREMENT=1048675 DEFAULT CHARSET=latin1")
+
+# cursor.execute("CREATE TABLE `imdbname` (\
+#   `id` int NOT NULL AUTO_INCREMENT,\
+#   `mainactor` varchar(45) NOT NULL,\
+#   `birthyear` varchar(30) DEFAULT NULL,\
+#   `deathyear` varchar(45) DEFAULT NULL,\
+#   `knownfortitles` varchar(45) DEFAULT NULL,\
+#   PRIMARY KEY (`id`)\
+# ) ENGINE=InnoDB AUTO_INCREMENT=1048756 DEFAULT CHARSET=latin1")
+
+# cursor.execute("CREATE TABLE `ratingtable` (\
+#   `id` int NOT NULL AUTO_INCREMENT,\
+#   `averagerating` int NOT NULL,\
+#   `numvotes` int NOT NULL,\
+#   PRIMARY KEY (`id`),\
+#   CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `basictable` (`id`) ON DELETE CASCADE ON UPDATE CASCADE\
+# ) ENGINE=InnoDB AUTO_INCREMENT=1048603 DEFAULT CHARSET=latin1")
+
+
+cnx = mysql.connector.connect(user='root', password='123456',
+                              host='127.0.0.1',
+                              port = "2589",
+                              database = "dbexample")
+
+"""Delete tables"""
+cursor = cnx.cursor()
+# try:
+#     cursor.execute("delete from imbdtitleakas")
+# except mysql.connector.Error as err:
+#     print("Failed creating database: {}".format(err))
+
+# try:
+#     cursor.execute("delete from basictable")
+# except mysql.connector.Error as err:
+#     print("Failed creating database: {}".format(err))
+
+
+# with open("ImdbTitleBasics.csv", 'r' , encoding = "utf8") as file:
+#     next(iter(file))
+#     csvreader = csv.reader(file)
+#     counter = 0
+#     cursor.execute("alter table basictable AUTO_INCREMENT = 1")
+#     for row in csvreader:
+#         counter+=1
+#         print(row)
+#         # if counter == 100:
+#         #   break
+#         try:
+#             cursor.execute("insert into basictable(moviename, movietype) values(%s, %s);", (row[2],row[1]))
+#         except mysql.connector.Error as err:
+#             print("Failed creating database: {}".format(err))
+#     cnx.commit()
+#     cursor = cnx.cursor()
+
+# with open("ImdbTitleRatings.csv", 'r' , encoding = "utf8") as file:
+#     next(iter(file))
+#     csvreader = csv.reader(file)
+#     counter = 0
+#     cursor.execute("alter table ratingtable AUTO_INCREMENT = 1")
+#     for row in csvreader:
+#         counter +=1
+#         print(row)
+#         # if counter == 10:
+#         #     break
+#         try:
+#             cursor.execute("insert into ratingtable(averagerating,numvotes) values(%s, %s);", (row[1],row[2]))
+#         except mysql.connector.Error as err:
+#             print("Failed creating database: {}".format(err))
+# cnx.commit()
+# cursor = cnx.cursor()
+
+# with open("ImdbName.csv", 'r' , encoding = "utf8") as file:
+#   next(iter(file))
+#   csvreader = csv.reader(file)
+#   counter = 0
+#   cursor.execute("alter table imdbname AUTO_INCREMENT = 1")
+#   for row in csvreader:
+#     counter+=1
+#     print(row)
+#     if counter == 10:
+#       break
+#     try:
+#         cursor.execute("insert into imdbname(mainactor,birthyear, deathyear,knownfortitles) values(%s, %s, %s,%s);", (row[1],row[2], row[3],row[5]))
+#     except mysql.connector.Error as err:
+#         print("Failed creating database: {}".format(err))
+
+cnx.commit()
+cursor = cnx.cursor()
+
+# with open("ImdbTitleAkas.csv", 'r' , encoding = "utf8") as file:
+#     next(iter(file))
+#     csvreader = csv.reader(file)
+#     counter = 0
+#     cursor.execute("alter table imbdtitleakas AUTO_INCREMENT = 1")
+#     for row in csvreader:
+#         counter += 1
+#         print(row)
+#         # if counter == 100:
+#         #     break
+#         try:
+#             cursor.execute("insert into imbdtitleakas(region,language) values(%s, %s);", (row[3],row[4]))
+#         except mysql.connector.Error as err:
+#             print("Failed creating database: {}".format(err))
+#
+# cnx.commit()
+# cnx.close()
+
